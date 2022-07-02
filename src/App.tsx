e57e0b8bbee3aeece8a96d293be0adcbaf2c46dc
Arthur Vergaças <arthur.martins@db.tec.br>
2022-07-02 11:55:09 -0300

    feat: create base folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>Navbar aqui</nav>
+      <Navbar />
       <Outlet />
       <footer>Footer aqui</footer>
     </>
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Navbar() {
+  return <>oioaisd</>;
+}
+
+export default Navbar;
